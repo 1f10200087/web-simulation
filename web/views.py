6c41f5c9095d6828a2f47a94fa6766b4ad6ef7be
s1f102001445 <s1f102001445@iniad.org>
2022-09-03 22:31:29 +0900

    fix 1
    メインページを作成(途中)

--- web/views.py
+++ web/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse('Hello Django')
+	return render(request, 'index.html')
+
+def sns(request):
+	return render(request, 'sns_troble.index')
