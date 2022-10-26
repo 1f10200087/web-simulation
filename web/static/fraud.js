bd9b71857dfa019b3ea2c54c89b71d10c9e917dd
Tomoki Yamazaki <s1f102000758@iniad.org>
2022-10-26 15:38:31 +0900

    動的なページの作成

--- web/static/fraud.js
+++ web/static/fraud.js
@@ -1,1 +1,23 @@
-console.log("Hello, world!");
+window.onload = function() {
+    window.alert("ご登録ありがとうございます。")
+}
+
+window.onload = function() {
+    console.log("Hello, world!");
+}
+
+/* アクセスした日時の表示 */
+var now = new Date();
+var target = document.getElementById("DateTimeDisp");
+
+var Year = now.getFullYear();
+var Month = now.getMonth()+1;
+var Date = now.getDate();
+var Hour = now.getHours();
+var Min = now.getMinutes();
+var Sec = now.getSeconds();
+
+console.log(Year);
+
+target.innerHTML = Year + "/" + Month + "/" + Date + " " + Hour + ":" + Min + ":" + Sec;
+
